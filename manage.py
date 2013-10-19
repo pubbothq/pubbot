a92dbbbf2f4eb9ad60a8aeba2c109ecb78a33229
John Carr <john.carr@unrouted.co.uk>
2013-10-19 23:32:51 +0100

    Update .travis.yml and fix example_project

--- manage.py
+++ manage.py
@@ -30,7 +30,7 @@ except ImportError:
 
 
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pubbot.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "example_project.settings")
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
 
