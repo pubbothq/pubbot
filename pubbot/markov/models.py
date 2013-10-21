d70ef681e5219e5893cb064dc4ef1cf1367f2f2d
John Carr <john.carr@unrouted.co.uk>
2013-10-21 10:09:03 +0100

    Flesh out markov generation

--- pubbot/markov/models.py
+++ pubbot/markov/models.py
@@ -15,33 +15,37 @@
 from django.db import models
 
 
-class WordManager(models.Manager):
-
-    def get_next_word(self):
-        return self.get_queryset().extra(select={'weight': 'random() * count'}).extra(order_by='weight')[0].right
-
-    def get_previous_word(self):
-        return self.get_queryset().extra(select={'weight': 'random() * count'}).extra(order_by='weight')[0].left
-
-
 class Word(models.Model):
-    objects = WordManager()
 
-    word = models.CharField(max_length=255, unique=True)
-    count = models.IntegerField(default=1)
+    word = models.CharField(max_length=510)
+    count = models.IntegerField(default=0)
 
     def __unicode__(self):
         return self.word
 
 
 class Chain(models.Model):
-    left = models.ForeignKey(Word, related_name='right')
-    right = models.ForeignKey(Word, related_name='left')
-    count = models.IntegerField(default=1)
+
+    left1 = models.ForeignKey(Word, related_name='left1')
+    left2 = models.ForeignKey(Word, related_name='left2')
+    right = models.ForeignKey(Word, related_name='right')
+    count = models.IntegerField(default=0)
+
+    def get_next_chain(self):
+        return Chain.objects.\
+            filter(left1=self.left2, left2=self.right).\
+            extra(select={'weight': 'random() * count'}).\
+            extra(order_by=['-weight'])[0]
+
+    def get_previous_chain(self):
+        return Chain.objects.\
+            filter(right=self.left2, left2=self.left1).\
+            extra(select={'-weight': 'random() * count'}).\
+            extra(order_by=['weight'])[0]
 
     def __unicode__(self):
-        return "%s -> %s" % (self.left, self.right)
+        return "%s -> %s -> %s" % (self.left1, self.left2, self.right)
 
     class Meta:
-        unique_together = ('left', 'right')
+        unique_together = ('left1', 'left2', 'right')
 
