246369174525f18fc8abe7f8ba5d599c63590dec
John Carr <john.carr@unrouted.co.uk>
2014-07-22 00:23:44 +0100

    Configure logging

--- example_project/settings.py
+++ example_project/settings.py
@@ -152,31 +152,22 @@ SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
 # more details on how to customize your logging configuration.
 LOGGING = {
     'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
     'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
     },
     'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
+        'django.db.backends': {
+            'handlers': ['console'],
+            'propagate': True,
+            'level': 'CRITICAL',
+        },
+        '': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
             'propagate': True,
         },
-    }
+    },
 }
-
-TEST_RUNNER = 'discover_runner.DiscoverRunner'
-
-BROKER_URL = 'amqp://'
-CELERY_RESULT_BACKEND = 'cache+memcached://127.0.0.1:11211/'
-CELERYD_POOL = 'gevent'
-
