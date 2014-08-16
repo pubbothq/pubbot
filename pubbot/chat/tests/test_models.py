1f688ed812890d823432d969050c06dfcde85bf3
John Carr <john.carr@unrouted.co.uk>
2014-08-16 18:24:47 +0100

    Fix flake8

--- pubbot/chat/tests/test_models.py
+++ pubbot/chat/tests/test_models.py
@@ -6,20 +6,20 @@ from pubbot.chat.models import Grouping
 
 class TestIterRandomGroupings(TestCase):
 
-       def setUp(self):
-           with Trainer() as trainer:
-               trainer.learn_string("I am happy to see you")
+    def setUp(self):
+        with Trainer() as trainer:
+            trainer.learn_string("I am happy to see you")
 
-       def test_iter_by_token(self):
-           groupings = list(Grouping.iter_random_groupings(["happy"]))
-           self.assertEqual(len(groupings), 1)
-           self.assertEqual(groupings[0].token1.token, "I")
-           self.assertEqual(groupings[0].token2.token, "am")
-           self.assertEqual(groupings[0].token3.token, "happy")
+    def test_iter_by_token(self):
+        groupings = list(Grouping.iter_random_groupings(["happy"]))
+        self.assertEqual(len(groupings), 1)
+        self.assertEqual(groupings[0].token1.token, "I")
+        self.assertEqual(groupings[0].token2.token, "am")
+        self.assertEqual(groupings[0].token3.token, "happy")
 
-       def test_iter_by_token(self):
-           groupings = list(Grouping.iter_random_groupings(["happiness"]))
-           self.assertEqual(len(groupings), 1)
-           self.assertEqual(groupings[0].token1.token, "I")
-           self.assertEqual(groupings[0].token2.token, "am")
-           self.assertEqual(groupings[0].token3.token, "happy")
+    def test_iter_by_stem(self):
+        groupings = list(Grouping.iter_random_groupings(["happiness"]))
+        self.assertEqual(len(groupings), 1)
+        self.assertEqual(groupings[0].token1.token, "I")
+        self.assertEqual(groupings[0].token2.token, "am")
+        self.assertEqual(groupings[0].token3.token, "happy")
