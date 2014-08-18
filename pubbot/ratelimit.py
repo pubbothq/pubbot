228e8ff1b0f4ce430fb65c6c37eb25e5261e8bbb
John Carr <john.carr@unrouted.co.uk>
2014-08-18 23:10:27 +0100

    Rate limit decorators: Parse strings like 4/5h

--- pubbot/ratelimit.py
+++ pubbot/ratelimit.py
@@ -5,6 +5,39 @@ import inspect
 from django.core.cache import caches
 
 
+def _get_rate(rate):
+    if "/" not in rate:
+        raise ValueError("'%s' is not a valid rate." % rate)
+
+    number, period = rate.split("/", 1)
+
+    if period[-1] not in ('s', 'm', 'h', 'd'):
+        raise ValueError("'%s' not recognised. Must be one of s, m, h or d." % period[-1])
+
+    try:
+        number = int(number)
+    except ValueError:
+        raise ValueError("Invalid frequency: '%s'" % number)
+
+    period, units = period[:-1], period[-1]
+    if period:
+        try:
+            period = int(period)
+        except ValueError:
+            raise ValueError("Invalid period: '%s'" % period)
+    else:
+        period = 1
+
+    period = period * {
+        's': 1,
+        'm': 60,
+        'h': 3600,
+        'd': 86400,
+    }[units]
+
+    return number, period
+
+
 def _get_key(fn, limit_by, *args, **kwargs):
     key = ['rate_limit']
     key.append(fn.__module__)
@@ -28,7 +61,7 @@ def _get_key(fn, limit_by, *args, **kwargs):
     return ":".join(key)
 
 
-def is_rate_limited(fn, limit_by=None, *args, **kwargs):
+def is_rate_limited(freq, period, fn, limit_by=None, *args, **kwargs):
     key = _get_key(fn, limit_by, *args, **kwargs)
 
     key
@@ -38,10 +71,12 @@ def is_rate_limited(fn, limit_by=None, *args, **kwargs):
 
 
 def track_rate_limit(rate, limit_by=None):
+    freq, period = _get_rate(rate)
+
     def decorator(fn):
         @wraps(fn)
         def _inner(*args, **kwargs):
-            if is_rate_limited(fn, limit_by, *args, **kwargs):
+            if is_rate_limited(freq, period, fn, limit_by, *args, **kwargs):
                 kwargs = dict(kwargs)
                 kwargs['rate_limited'] = True
             return fn(*args, **kwargs)
@@ -50,10 +85,12 @@ def track_rate_limit(rate, limit_by=None):
 
 
 def enforce_rate_limit(rate, limit_by=None):
+    freq, period = _get_rate(rate)
+
     def decorator(fn):
         @wraps(fn)
         def _inner(*args, **kwargs):
-            if is_rate_limited(fn, limit_by, *args, **kwargs):
+            if is_rate_limited(freq, period, fn, limit_by, *args, **kwargs):
                 return
             return fn(*args, **kwargs)
         return _inner
