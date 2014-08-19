928b1111f6651429201f37171eeaeed6fdd2d4a8
John Carr <john.carr@unrouted.co.uk>
2014-08-19 23:32:08 +0100

    Add docstrings and annotations to stop coveralls.io from saying empty files have 0% coverage

--- pubbot/squeezecenter/__init__.py
+++ pubbot/squeezecenter/__init__.py
@@ -11,3 +11,5 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+""" Support for interacting with SqueezeCenter and SlimDevices """
