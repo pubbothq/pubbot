83fcd4a12a1d6bc17a046785738fa06d64fb6dac
John Carr <john.carr@unrouted.co.uk>
2014-08-24 16:05:33 +0100

    We expect manage.py to be as bin/pubbot, so remove __name__ stuff

--- pubbot/manage.py
+++ pubbot/manage.py
@@ -24,7 +24,3 @@ def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "example_project.settings")
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
-
-
-if __name__ == "__main__":
-    main()
