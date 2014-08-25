cb0595a5078b5fea047f8712c79cc8a8ecae7a51
John Carr <john.carr@unrouted.co.uk>
2014-08-25 22:22:30 +0100

    More test coverage for chat 'utter' mgmt command

--- pubbot/chat/tests/test_management_commands_utter.py
+++ pubbot/chat/tests/test_management_commands_utter.py
@@ -2,6 +2,7 @@ import StringIO as StringIO
 
 from django.test import TestCase
 from django.core.management import call_command
+from django.core.management.base import CommandError
 
 from pubbot.chat.receivers import learn
 
@@ -11,7 +12,15 @@ class TestUtter(TestCase):
     def setUp(self):
         learn(None, content="Happiness is important")
 
-    def test_read_irssi_log(self):
+    def test_no_args(self):
+        buf = StringIO.StringIO()
+        self.assertRaises(CommandError, call_command, "utter", stdout=buf)
+
+    def test_too_many_args(self):
+        buf = StringIO.StringIO()
+        self.assertRaises(CommandError, call_command, "utter", "Chocolate", "is", "important", stdout=buf)
+
+    def test_utter(self):
         buf = StringIO.StringIO()
         call_command("utter", "Chocolate is important", stdout=buf)
         self.assertEqual(buf.getvalue(), "Happiness is important\n")
