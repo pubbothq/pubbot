0faf6c5a86b6bf2c908fe16d7a9782fe78857650
John Carr <john.carr@unrouted.co.uk>
2014-09-03 11:58:55 +0100

    Fix twitter broadcasting handler

--- pubbot/twitter/receivers.py
+++ pubbot/twitter/receivers.py
@@ -6,14 +6,14 @@ from pubbot.twitter.signals import tweet
 
 
 @receiver(tweet)
-def broadcast_tweet(self, text, user, **kwargs):
+def broadcast_tweet(sender, text, user, id_str, **kwargs):
     if config.TWITTER_BROADCAST_FOLLOW:
         if user['screen_name'] not in config.TWITTER_BROADCAST_FOLLOW.split(","):
             return
 
     say(
         content="\n".join([
-            "https://twitter.com/%s/status/%s" % (user['screen_name'], id),
+            "https://twitter.com/%s/status/%s" % (user['screen_name'], id_str),
             "[ %s: %s ]" % (user['screen_name'], text.replace("\n", ""))
         ]),
         tags=['twitter_stream'],
