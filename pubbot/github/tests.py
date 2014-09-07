fc86bd363257cb2b3133f8d4f37b163713d0e591
John Carr <john.carr@unrouted.co.uk>
2014-09-07 21:46:33 +0100

    Tests for pubbot.github.receivers

---
+++ pubbot/github/tests.py
@@ -0,0 +1,44 @@
+import mock
+import unittest
+
+from pubbot.github import receivers
+
+
+class TestPushToCommit(unittest.TestCase):
+
+    def test_push(self):
+        with mock.patch("pubbot.github.receivers.commit") as commit:
+            payload = mock.Mock()
+            payload.payload = {
+                "commits": [
+                    {"distinct": False},
+                    {"distinct": True, "sha": "abc", "message": "hello", "author": {"name": "Pubbot"}},
+                ]
+            }
+            receivers.push_to_commit(None, payload)
+
+            commit.send.assert_called_with(
+                revision="abc",
+                message="hello",
+                committer="Pubbot",
+            )
+
+
+class TestPushToChat(unittest.TestCase):
+
+    def test_push(self):
+        with mock.patch("pubbot.github.receivers.say") as say:
+            payload = mock.Mock()
+            payload.repo = ('pubbothq', 'pubbot')
+            payload.payload = {
+                "commits": [
+                    {"distinct": False},
+                    {"distinct": True, "sha": "abc", "message": "hello", "author": {"name": "Pubbot"}},
+                ]
+            }
+            receivers.push_to_chat(None, payload)
+
+            say.assert_called_with(
+                content="\x0303Pubbot \x0302pubbothq/pubbot \x0310abc\x0314\x0f: hello ...",
+                tags=['github:pubbothq/pubbot']
+            )
